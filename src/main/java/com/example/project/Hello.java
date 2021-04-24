a9fa54dd3616637a690d60cd4bf5a51138319fee
Ricardo Sanchez <ricardo.profe.informatica@gmail.com>
2021-04-24 22:21:17 +0200

    añadido codigo

---
+++ src/main/java/com/example/project/Hello.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+public class Hello {
+
+	public static void main(final String[] args) {
+		System.out.println("NOMBRES DE LOS MIEMBROS DEL EQUIPO");
+		System.out.println("----------------------------------");
+		//Escribid vuestros nombres con un println
+	}
+
+}
+
